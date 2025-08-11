ddab6345efc0d3d365606d4389a423d5ea2e4f7a
jayasreepm95 <jayasreepm95@gmail.com>
2025-08-12 01:13:53 +0530

    first new-feature to be added

--- pattern2.py
+++ pattern2.py
@@ -1,2 +1,11 @@
 for n in range(1,6):
     print("* " * n)
+
+    rows = 5
+    for i in range(0, rows):
+        # nested loop for each column
+        for j in range(0, i + 1):
+            # print star
+            print(j, end=' ')
+        # new line after each row
+        print("\r")
